d575f7d42e41a2f7c834deecd0e6021a0fc4ea4f
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-12-06 00:06:05 +0900

    記事のスキーマの定義とマイグレーション作成

---
+++ backend/prisma/migrations/20241205143251_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Article" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "description" TEXT,
+    "tags" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Article_pkey" PRIMARY KEY ("id")
+);
