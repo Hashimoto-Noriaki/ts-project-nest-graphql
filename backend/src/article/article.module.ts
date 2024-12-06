6ea39aca15c1a21151d33b4c5f6f2b3d5bf754f0
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-12-06 12:23:46 +0900

    タスクの作成と一覧取得

--- backend/src/article/article.module.ts
+++ backend/src/article/article.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ArticleService } from './article.service';
 import { ArticleResolver } from './article.resolver';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [ArticleService, ArticleResolver],
 })
 export class ArticleModule {}
